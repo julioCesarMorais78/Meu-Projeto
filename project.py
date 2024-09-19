204a5f07dacc2612390e45d3ecfca43302a3f338
julioCesarMorais78 <153240019+julioCesarMorais78@users.noreply.github.com>
2024-09-19 15:50:30 +0000

    Pending changes exported from your codespace

---
+++ project.py
@@ -0,0 +1,8 @@
+# Iniciando primeiro projeto utilizando o GITHub
+import math
+
+print('Estou iniciando o meu primeiro projeto em python no GIT Hub')
+
+num1 = 9
+resp = math.sqrt(num1)
+print(resp)
