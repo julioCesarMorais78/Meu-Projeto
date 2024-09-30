bc006c4b20d02a404551a542ee0742190e3d439c
julioCesarMorais78 <153240019+julioCesarMorais78@users.noreply.github.com>
2024-09-30 21:39:39 +0000

    Inserindo uma nova linha no programa e fazendo um novo commit

---
+++ project.py
@@ -0,0 +1,11 @@
+# Iniciando primeiro projeto utilizando o GITHub
+import math
+
+print('Estou iniciando o meu primeiro projeto em python no GIT Hub')
+
+num1 = 9
+resp = math.sqrt(num1)
+print(resp)
+
+print('Inserindo uma nova linha para verificar e aprender a fazer um commit')
+
