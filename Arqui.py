d234a09028ea9b2b3a7e5280848f17f789d36e46
julioCesarMorais78 <153240019+julioCesarMorais78@users.noreply.github.com>
2024-11-21 21:40:21 +0000

    inserido mais uma linha de print.

--- Arqui.py
+++ Arqui.py
@@ -1,3 +1,5 @@
 # inserido mais um arquivo na plataforma fo GIT Hub
 
-print('Acabei de nseri um novo arquivo na plataforma do Github')
+print('Acabei de inseri um novo arquivo na plataforma do Github')
+
+print("Acabei de inserir uma outra linha no programa - Teste")
